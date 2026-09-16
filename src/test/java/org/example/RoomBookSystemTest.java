25b6b984cab2724c0b1c621c4720495e983eb328
unknown <akinolaoyindamola683@gmail.com>
2026-09-16 11:51:06 -0700

    TDD for Hotel system: Book a room, check is availabe, cencelBooking

--- src/test/java/org/example/RoomBookSystemTest.java
+++ src/test/java/org/example/RoomBookSystemTest.java
@@ -1,4 +1,31 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RoomBookSystemTest {
-  
+    RoomBookSystem roomSystem =  new RoomBookSystem(101, "Singel", 10000.0, true);
+
+    @Test
+    void checkIsAvaliable() {
+        assertEquals(true, roomSystem.isAvaliable());
+
+    }
+
+    @Test
+    void bookARoom() {
+        roomSystem.bookARoom(101,2);
+        roomSystem.checkIsAvaliable();
+        assertEquals(false, roomSystem.isAvaliable());
+        assertEquals(101, roomSystem.getRoomNumber());
+
+    }
+
+    @Test
+    void cancelBooking() {
+        roomSystem.cancelBooking(101);
+        assertEquals(true, roomSystem.isAvaliable());
+    }
+
 }
